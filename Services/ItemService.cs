7ad9bca6efaff66d167650552b994f6e48b57f38
piyumiima <piyumiimasha9@gmail.com>
2026-05-18 19:55:23 +0530

    Enhance ItemService to handle related data deletion

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -9,11 +9,22 @@ namespace Tasty_Treat_be.Services
     public class ItemService : IItemService
     {
         private readonly IItemRepository _itemRepository;
+        private readonly IItemFlavourRepository _itemFlavourRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IOrderItemRepository _orderItemRepository;
         private readonly IMapper _mapper;
 
-        public ItemService(IItemRepository itemRepository, IMapper mapper)
+        public ItemService(
+            IItemRepository itemRepository,
+            IItemFlavourRepository itemFlavourRepository,
+            IReviewRepository reviewRepository,
+            IOrderItemRepository orderItemRepository,
+            IMapper mapper)
         {
             _itemRepository = itemRepository;
+            _itemFlavourRepository = itemFlavourRepository;
+            _reviewRepository = reviewRepository;
+            _orderItemRepository = orderItemRepository;
             _mapper = mapper;
         }
 
@@ -82,6 +93,22 @@ namespace Tasty_Treat_be.Services
 
         public async Task<bool> DeleteAsync(int id)
         {
+            var item = await _itemRepository.GetByIdAsync(id);
+            if (item == null) return false;
+
+            // Remove related records that reference this item
+            var flavours = await _itemFlavourRepository.GetByItemIdAsync(id);
+            foreach (var f in flavours)
+                await _itemFlavourRepository.DeleteAsync(f.ItemFlavourId);
+
+            var reviews = await _reviewRepository.GetByItemIdAsync(id);
+            foreach (var r in reviews)
+                await _reviewRepository.DeleteAsync(r.ReviewId);
+
+            var orderItems = await _orderItemRepository.FindAsync(oi => oi.ItemId == id);
+            foreach (var oi in orderItems)
+                await _orderItemRepository.DeleteAsync(oi.OrderItemId);
+
             return await _itemRepository.DeleteAsync(id);
         }
     }
